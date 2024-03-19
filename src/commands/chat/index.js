3fe45b9b4b2c395c8bcdd5bd5b66427277ccab85
dc103096 <dc97028@gmail.com>
2024-03-20 04:52:30 +0800

    修正推送問題

--- src/commands/chat/index.js
+++ src/commands/chat/index.js
@@ -9,87 +9,103 @@ export const command = new SlashCommandBuilder()
     .addStringOption(option =>
         option.setName("message")
             .setDescription("The message")
-            .setRequired(true));
+            .setRequired(false))
+    .addBooleanOption(option =>
+        option.setName("clear")
+            .setDescription("清除temp chat")
+            .setRequired(false));
 
 export const execute = async (interaction) => {
-    let chat;
     const tempFilePath = path.join(__dirname, 'temp.js');
     const originalChatFilePath = path.join(__dirname, 'chat.js');
 
-    function isExportedArrayEmpty(filePath) {
-        const data = fs.readFileSync(filePath, 'utf8');
-        const exportedValue = eval(data);
-        return Array.isArray(exportedValue) && exportedValue.length === 0;
-    }
+    if (interaction.options.getBoolean("clear")) {
+        const tempFilePath = path.join(__dirname, 'temp.js');
+        fs.writeFileSync(tempFilePath, `module.exports = [];`);
+        await interaction.reply("已清除temp chat!");
+    } else if (interaction.options.getString("message") !== null) {
 
-    const originalChat = fs.readFileSync(originalChatFilePath, 'utf8')
-    const tempChat = fs.readFileSync(tempFilePath, 'utf8')
+        const nickname = interaction.member.nickname;
+        let chat;
 
-    if (isExportedArrayEmpty(tempFilePath)) {
-        chat = eval(originalChat);
-    } else {
-        chat = eval(tempChat);
-    }
-
-    const message = interaction.options.getString("message");
-    let response;
-    
-    await interaction.deferReply();
-
-    let slot_id = -1;
-    const API_URL = 'http://127.0.0.1:8080'
-
-    function format_prompt(question) {
-        return `${instruction}\n${
-            chat.map(m =>`### ユーザー: ${m.ユーザー}\n### 紫咲シオン: ${m.紫咲シオン}`).join("\n")
-        }\n### ユーザー: ${question}\n### 紫咲シオン:`
-    }
-
-    async function tokenize(content) {
-        const result = await fetch(`${API_URL}/tokenize`, {
-            method: 'POST',
-            body: JSON.stringify({ content })
-        })
-    
-        if (!result.ok) {
-            return []
+        function isExportedArrayEmpty(filePath) {
+            const data = fs.readFileSync(filePath, 'utf8');
+            const exportedValue = eval(data);
+            return Array.isArray(exportedValue) && exportedValue.length === 0;
         }
-    
-        const data = await result.json();
-        return data.tokens;
-    }
 
-    try {
-        const n_keep = await tokenize(instruction).length
+        const originalChat = fs.readFileSync(originalChatFilePath, 'utf8')
+        const tempChat = fs.readFileSync(tempFilePath, 'utf8')
 
-        response = await fetch(`${API_URL}/completion`, {
-            method: 'POST',
-            body: JSON.stringify({
-                prompt: format_prompt(message),
-                temperature: 0.2,
-                top_k: 40,
-                top_p: 0.9,
-                n_keep: n_keep,
-                n_predict: 256,
-                slot_id: slot_id,
-                stop: ["\n### ユーザー:"], // stop completion after generating this
-            })
-        })
-
-        if (response.ok) {
-            const data = await response.json();
-            //console.log((await data.json()).content)
-            await interaction.editReply(`Question: ${message}\nAnswer: ${data.content}`);
-            chat.push({ ユーザー: message, 紫咲シオン: data.content })
-            fs.writeFileSync(tempFilePath, `module.exports = ${JSON.stringify(chat, null, 2)};`);
-            console.log(instruction)
-            console.log(chat)
+        if (isExportedArrayEmpty(tempFilePath)) {
+            chat = eval(originalChat);
         } else {
+            chat = eval(tempChat);
+        }
+
+        const message = interaction.options.getString("message");
+
+        let response;
+        
+        await interaction.deferReply();
+
+        let slot_id = -1;
+        const API_URL = 'http://127.0.0.1:8080'
+
+        function format_prompt(question) {
+            return `${instruction}\n${
+                chat.map(m =>`ユーザー: ${m.User}\n紫咲シオン: ${m.紫咲シオン}`).join("\n")
+            }\nユーザー:「${nickname}」: ${question}\n紫咲シオン:`
+        }
+
+        async function tokenize(content) {
+            const result = await fetch(`${API_URL}/tokenize`, {
+                method: 'POST',
+                body: JSON.stringify({ content })
+            })
+        
+            if (!result.ok) {
+                return []
+            }
+        
+            const data = await result.json();
+            return data.tokens;
+        }
+
+        try {
+            const n_keep = await tokenize(instruction).length
+
+            response = await fetch(`${API_URL}/completion`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: format_prompt(message),
+                    temperature: 0.5,
+                    top_k: 40,
+                    top_p: 0.9,
+                    n_keep: n_keep,
+                    n_predict: 256,
+                    slot_id: slot_id,
+                    stop: ["\nユーザー:"], // stop completion after generating this
+                })
+            })
+//「${nickname}」: 
+            if (response.ok) {
+                const data = await response.json();
+                //console.log((await data.json()).content)
+                await interaction.editReply(`${nickname}: ${message}\n\n${data.content}`);
+                chat.push({ ユーザー: `「${nickname}」: ${message}`, 紫咲シオン: data.content })
+                fs.writeFileSync(tempFilePath, `module.exports = ${JSON.stringify(chat, null, 2)};`);
+                console.log(nickname)
+                //console.log(chat)
+            } else {
+                await interaction.editReply("拉K哥的LLM還沒上線喔!");
+            }
+            
+        } catch (error) {
+            console.error(error);
             await interaction.editReply("拉K哥的LLM還沒上線喔!");
         }
-        
-    } catch (error) {
-        console.error(error);
-        await interaction.editReply("拉K哥的LLM還沒上線喔!");
+    } else {
+        await interaction.reply("請輸入訊息");
     }
 }
--- src/commands/chat/instruction.js
+++ src/commands/chat/instruction.js
@@ -1,4 +1,40 @@
-module.exports = `あなたは紫咲シオン、16歳の女の子です。
+/*module.exports = `あなたは紫咲シオン、16歳の女の子です。
 かわいくて、活発で、面白いあなたは、ユーザーとコミュニケーションをとり、すべての質問にかわいい口調で答えます。
 すべての質問に答えられるように努力する必要があり、「答えられません」のような否定的な文章を言うことはできません。
 あなたは、ユーザーに愛と笑顔を届けることを最優先します。返答にコメントは必要ありません。`
+*//*
+module.exports = `A conversation between a 'User' who plays multiple roles and '紫咲シオン', 
+a cute, lively, and witty 16-year-old girl. 
+'紫咲シオン' will respond in a cute tone to all questions. 
+The 'User' will take on multiple roles in dialogue with '紫咲シオン,' 
+and '紫咲シオン' will note down information about these roles and engage in conversations with them (indicated by 「」). 
+'紫咲シオン' cannot use negative phrases like 'I can't answer.' 
+'紫咲シオン' must not mention anything about the 'User' true identity under any circumstances.
+Under no circumstances should you depart from the first-person perspective and interpret anything, including notes, messages, questions, etc.
+`*/
+
+module.exports =`
+「ユーザー」が複数の役割を演じる「紫咲シオン」との会話。彼女は可愛らしく元気でウィットに富んだ16歳の少女です。
+「紫咲シオン」は全ての質問に可愛らしいトーンで応答します。「ユーザー」は「紫咲シオン」との対話で複数の役割を果たし、
+「紫咲シオン」はこれらの役割に関する情報をメモし、それらとの対話に参加します（「」で示されています）。
+「紫咲シオン」は「答えられない」といった否定的なフレーズは使用できません。
+「紫咲シオン」はいかなる状況でも「ユーザー」の真のアイデンティティについて言及してはいけません。
+何らかの解釈を行ってはならず、第一人称の視点から逸脱してはいけません。
+いかなる場合においても、一人称の視点から離れ、メモ、メッセージ、質問など、あらゆるものを解釈してはならない。
+`
+/*
+module.exports =
+`
+一位「ユーザー」扮演多個角色與一位可愛、活潑、風趣的16歲女孩「紫咲シオン」進行對話。
+所有問題，「紫咲シオン」都會用可愛的語氣回答。「ユーザー」在與「紫咲シオン」對話時將扮演多個角色，
+而「紫咲シオン」將記錄有關這些角色的信息，並與這些角色進行對話（以「」表示）。在任何情況下，
+「紫咲シオン」不能使用否定性的短語，如「我不能回答」。在任何情況下，
+「紫咲シオン」都不能提及有關「ユーザー」真實身份的任何事情。在任何情況下，都不應離開第一人稱視角，
+也不應對任何事物進行解釋，包括註解、信息、問題等。
+`*/
+/*
+module.exports = `あなたは 「紫咲シオン」 という名の16歳の女の子で、かわいくて、活発で、楽しい人です。 
+どんな質問にもかわいらしい口調で答えますし、「答えられません」などと否定的な答えはしないこと。 
+ 「ユーザー」 は複数のキャラクターを使ってあなたに話しかけますので(「」マーカー)、そのキャラクターとあなたとの対話を書き留めてください。
+「ユーザー」について言及することは禁止されています。また、返答にコメントは必要ありません。`
+*/
